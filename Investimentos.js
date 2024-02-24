d2904df8cff1769836ec37a07db7e7924ae07c86
GustavoDMarco <gustavo.marco@unesp.br>
2024-02-24 05:04:36 -0300

    Algumas alterações e incluindo funções para DY

--- Investimentos.js
+++ Investimentos.js
@@ -1,7 +1,8 @@
-function ResultadoMês(mesReferencia, mesPagamento) {            //Objeto para o mês
+function ResultadoMês(mesReferencia, mesPagamento, totalInvestidoMes) {            //Objeto para o mês
     return{
         mesReferencia,
         mesPagamento,
+        totalInvestidoMes,
         recebidosMes: [],
 
         Mes(){                                                  //Imprime meses de referência e pagamento
@@ -13,66 +14,82 @@ function ResultadoMês(mesReferencia, mesPagamento) {            //Objeto para o
             this.recebidosMes = recebidosMes
         },
 
-        imprimirSomaMes(){                                      //Soma os dividendos de todos os meses e imprime
+        somaMes(){                                              //Soma os dividendos do mes
             let soma = 0
             this.recebidosMes.forEach( function(valor){         //Entro dentro do array e em cada indice pego o valor
                 soma = soma + valor
            })
+           return soma
+        },
+        
+        imprimirSomaMes(){                                      //Imprime soma dos dividedendos do mes
+            let soma = 0
+            this.recebidosMes.forEach( function(valor){         //Entro dentro do array e em cada indice pego o valor
+                soma = soma + valor
+            })
             console.log(soma)
         },
 
-        somaTotal(){                                            //Soma os dividendos de todos os meses
-            let soma = 0
-            this.recebidosMes.forEach( function(valor){
-                soma = soma + valor
-           })
-           return soma
+        dividendYieldMes(){                                     //Calcula o dividend yield e imprime
+            let dy = (this.somaMes() / this.totalInvestidoMes)*100
+            console.log("DY = " + dy)
         }
 
     }
 }
 
-function ResultadoAno(ano) {                                    //Objeto para relacionar todos os meses do ano
+function ResultadoAno(ano, totalInvestidoAno) {               //Objeto para relacionar todos os meses do ano
     return{
-        ano: ano,
+        ano,
+        totalInvestidoAno,
         Meses: [],
 
-        JuntaMeses(...Meses){                 //Junta todos os objetos de meses dentro de um array (array de objetos)
+        JuntaMeses(...Meses){                                 //Junta todos os objetos de meses dentro de um array (array de objetos)
             this.Meses=Meses
         },
 
-        TotalRecebido(Meses){                                   //Soma os dividendos de todos os meses
+        TotalRecebido(){                                      //Soma os dividendos de todos os meses
             let Total = 0
             this.Meses.forEach(function(valor) {
-                Total = Total + valor.somaTotal()
+                Total = Total + valor.somaMes()
             })
             return Total
         },
 
-        relatorio(mesReferencia, mesPagamento, Meses){          //Imprime o relatorio final do ano
+        dividendYieldAno(){                                     //Calcula dividend yield do ano       
+            let dy =0
+            dy = (this.TotalRecebido() / this.totalInvestidoAno)*100
+            return dy
+        },
+
+        relatorio(){                                            //Imprime o relatorio final do ano
             console.log("Resultado " + ano)
             
             this.Meses.forEach(function(valor) {
-                    console.log("Mês Referência: " + valor.mesReferencia + " | Mês Pagamento: " + valor.mesPagamento + " = " + valor.somaTotal())
+                    console.log("Mês Referência: " + valor.mesReferencia + " | Mês Pagamento: " + valor.mesPagamento + " = " + valor.somaMes())
             })
-
             console.log("TOTAL RECEBIDO: " + this.TotalRecebido())
+
+            console.log(this.dividendYieldAno())
         }
     }
 }
 
-const mes1 = ResultadoMês("Dezembro", "Janeiro")            //Criando objeto do mes. Passar mês referência e pagamento
+
+const mes1 = ResultadoMês("Dezembro", "Janeiro",5000)       //Criando objeto do mes. Passar mês referência e pagamento e valor final investido.
 mes1.Mes()
 mes1.dividendos(10.75,12.5,3.3,5.45,15)                     //Passar dividendos do mês
 mes1.imprimirSomaMes()                                      //Imprime soma dos dividendos do mês
+mes1.dividendYieldMes()                                     //Imprime dividend yield do mês
 console.log("---------------------")
 
-const mes2 = ResultadoMês("Janeiro","Fevereiro")
+const mes2 = ResultadoMês("Janeiro","Fevereiro", 6000)
 mes2.Mes()
 mes2.dividendos(12,8,3.3,5.75,12.1,15)              
 mes2.imprimirSomaMes()
+mes2.dividendYieldMes()
 console.log("---------------------")
 
-const Ano2024 = ResultadoAno(2024)                          //Cria objeto do Ano. Passar ano
+const Ano2024 = ResultadoAno(2024, 10000)                          //Cria objeto do Ano. Passar ano e valor final investido.
 Ano2024.JuntaMeses(mes1,mes2)
 Ano2024.relatorio()
